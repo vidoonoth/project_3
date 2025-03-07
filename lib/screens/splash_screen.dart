00b2fb580a3b1adf0ece20c7b3cce5ca2ce5e476
vidoonoth <alfinvidoo@gmail.com>
2025-03-07 15:02:29 +0700

    ubah tampilan splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -31,27 +31,36 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+        child: Row(
+          mainAxisSize:
+              MainAxisSize.min, // Tambahkan ini agar Row tidak memenuhi layar
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SvgPicture.asset(
-              'assets/perpus_logo.svg',
-              width: 80,
-              height: 80,
-            ), // Logo
-            SizedBox(height: 12),
-            Text(
-              'Perpustakaan Indramayu',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF2196F3),
-              ),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Sedang memuat...',
-              style: TextStyle(fontSize: 14, color: Colors.grey),
+            SvgPicture.asset('assets/perpus_logo.svg', width: 70, height: 70),
+            SizedBox(width: 8),
+            Column(
+              mainAxisSize:
+                  MainAxisSize
+                      .min, // Tambahkan ini agar Column tidak memenuhi layar
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Perpustakaan',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF2196F3),
+                  ),
+                ),
+                Text(
+                  'Indramayu',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF2196F3),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
